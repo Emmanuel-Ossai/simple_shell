6ffe4ab558a922c53cd3c564f7fb25a9ebc8cad7
Emmanuel Ossai <emmanuelbestossai@gmail.com>
2023-05-25 01:23:56 +0000

    group simple shell task

--- main.c
+++ main.c
@@ -6,7 +6,6 @@
  * @av: the argument vector
  * Return: 0
  **/
-
 int main(int ac, char **av)
 {
 	info_t info[] = { INFO_INIT };
@@ -17,15 +16,14 @@ int main(int ac, char **av)
 			: "=r" (fd)
 			: "r" (fd));
 
+
 	if (ac == 2)
 	{
 		fd = open(av[1], O_RDONLY);
 		if (fd == -1)
 		{
 			if (errno == EACCES)
-			{
 				exit(126);
-			}
 			if (errno == ENOENT)
 			{
 				_eputs(av[0]);
@@ -38,11 +36,9 @@ int main(int ac, char **av)
 			return (EXIT_FAILURE);
 		}
 		info->readfd = fd;
-		hsh(info, av);
-		return (EXIT_SUCCESS);
 	}
-
+	environ_list(info);
+	read_history(info);
 	hsh(info, av);
 	return (EXIT_SUCCESS);
 }
-
